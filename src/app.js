1147195251429ec38c7054727c2005bd2224834e
atxoupi <andres.hermelo@gmail.com>
2022-05-09 19:09:43 +0000

    Excuse creado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let indexWho = Math.floor(Math.random() * 3);
+  let indexWhat = Math.floor(Math.random() * 3);
+  let indexWhen = Math.floor(Math.random() * 4);
+  let miExcusa =
+    who[indexWho] + " " + what[indexWhat] + " my home work " + when[indexWhen];
+  console.log(miExcusa);
+  document.querySelector("#excusa").innerHTML = miExcusa;
 };
